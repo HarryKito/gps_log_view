e08241b48a847a39c2ad2d50fa7fe9c3080f4d3b
harrykito <korhary@gmail.com>
2024-09-26 01:36:08 +0900

    update the datatips and update the view of errors for the HDop

--- research.m
+++ research.m
@@ -1,3 +1,6 @@
+% Parameters control by User
+hdop_threshold = 1.5;
+
 hdop = [];
 lat = [];
 lng = [];
@@ -9,8 +12,6 @@ bin = ardupilotreader(binary_log_file);
 
 msg = readMessages(bin);
 
-% Read data between 2min 34sec and 20min.
-% Test 1번 자료 비행 = 2분부터 기록되어있음...
 d1 = duration([0 0 00],'Format','hh:mm:ss.SSSSSS');
 d2 = d1 + duration([0 20 00],'Format','hh:mm:ss.SSSSSS');
 
@@ -25,21 +26,28 @@ Lng = positions.Lng;
 Alt = positions.Alt;
 
 % 
-% HDop로 계산하기...
+% Expect with HDop...
 % 
 hdop_normalized = (hdop - min(hdop)) / (max(hdop) - min(hdop));
 cmap = [hdop_normalized, 1 - hdop_normalized, zeros(length(hdop_normalized), 1)];
 
 figure;
-% plot3(Lng, Lat, Alt, 'o-', 'Color', [0.5 0.5 0.5], 'LineWidth', 1.5);
 hold on;
 grid on;
 for i = 1:length(Lng)-1
     plot3(Lng(i:i+1), Lat(i:i+1), Alt(i:i+1), 'Color', cmap(i,:), 'LineWidth', 1.5);
 end
 
+% Take off (Green filled circle)
 scatter3(Lng(1), Lat(1), Alt(1), 100, 'g', 'filled');
+% Land (Red 'X')
 scatter3(Lng(end), Lat(end), Alt(end), 100, 'r', 'x', 'LineWidth', 2);
+
+for i = 1:length(high_hdop_change_indices)
+    idx = high_hdop_change_indices(i);
+    scatter3(Lng(idx), Lat(idx), Alt(idx), 100, 'm', 'filled');
+    text(Lng(idx), Lat(idx), Alt(idx), sprintf('%d', i), 'FontSize', 10, 'Color', 'k', 'HorizontalAlignment', 'left', 'VerticalAlignment', 'bottom');
+end
 
 xlabel('Longitude');
 ylabel('Latitude');
@@ -47,3 +55,7 @@ zlabel('Altitude');
 title('GPS Position with HDop');
 
 view(45, 30);
+
+% Change the Datatip for this project
+dcm = datacursormode(gcf);
+set(dcm, 'UpdateFcn', @(obj, event_obj) GPS_log_view_Datatip(obj, event_obj, Lng, Lat, Alt, hdop));
